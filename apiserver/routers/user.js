bc4012012f7cfefac75fc6bbe9797cdd7d860019
Rainbow <rainbow@miamusic.com>
2019-06-13 00:48:32 +0800

    '前端页面的实现'

--- apiserver/routers/user.js
+++ apiserver/routers/user.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const UserModel = require('../model/user');
 
@@ -35,4 +36,52 @@ router.post('/sign-up',(req,res)=>{
             })
         }
     })
-})
+});
+
+// /api/user/sign-in
+
+router.post('/sign-in',(req,res)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+
+    UserModel.findOne({
+        username,
+
+    }).then(data =>{
+        console.log(data); //data 就是找到的这个记录的对象
+        if(data){
+        //    比较密码
+            let isOk = bcrypt.compareSync(password,data.password);
+
+            if(isOk){
+                res.send({
+                    code:0,
+                    msg:'ok'
+                })
+
+            }else{
+                // 密码错误
+                res.send({
+                    code:-1,
+                    msg: '用户名或密码错误'
+                })
+            }
+        }else{
+            // 用户名错误
+            res.send({
+                code: -1,
+                msg: '用户名或密码错误'
+            })
+        }
+
+    }).catch(error=>{
+        console.log(error.message);
+
+        res.send({
+            code:-1,
+            msg:'网络异常，请稍后重试...'
+        })
+    })
+});
+
+module.exports = router;
